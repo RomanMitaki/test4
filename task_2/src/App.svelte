828764bd66d588ffc80f5219a68149385cbd235d
RomanMitaki <r.mitaki@yandex.ru>
2023-10-11 15:27:15 +0300

    add styles

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -48,7 +48,6 @@
 
   function handleCurrencyChangeFrom(e) {
     fetchRates(from);
-
   }
 
   onMount(async () => {
@@ -63,15 +62,15 @@
 
 <main class="calc">
   <div class="calc__wrapper_title">
-    <h1 class="calc__title">Currency Converter</h1>
+    <h1>Currency Converter</h1>
   </div>
-  <div class="calc__wrapper_side">
-    <div class="calc__wrapper_side-left">
+  <div class="calc__wrapper_sides">
+    <div class="calc__wrapper_side">
       <div class="calc__side">
-        <label for="leftAmount">Amount</label>
+        <label class="calc__label" for="leftAmount">Amount of {from}</label>
         <input
                 id="leftAmount"
-                class=""
+                class="calc__input"
                 name="leftAmount"
                 type="number"
                 min="0"
@@ -82,11 +81,11 @@
                 on:focus={handleFocus}
         />
       </div>
-      <div class="">
-        <label for="fromCurrency">Convert From:</label>
+      <div class="calc__side">
+        <label class="calc__label" for="fromCurrency">Convert From:</label>
         <input
                 id="fromCurrency"
-                class=""
+                class="calc__input"
                 name="fromCurrency"
                 maxlength="3"
                 value={from}
@@ -102,12 +101,12 @@
         </datalist>
       </div>
     </div>
-    <div class="calc__wrapper_side-right">
+    <div class="calc__wrapper_side">
       <div class="calc__side">
-        <label for="rightAmount">Amount</label>
+        <label class="calc__label" for="rightAmount">Amount of {to}</label>
         <input
                 id="rightAmount"
-                class=""
+                class="calc__input"
                 name="rightAmount"
                 type="number"
                 min="0"
@@ -117,33 +116,106 @@
                 on:input={handleChange}
                 on:focus={handleFocus}
         />
-        <div class="">
-          <label for="toCurrency">Convert To:</label>
-          <input
-                  id="toCurrency"
-                  class=""
-                  name="toCurrency"
-                  maxlength="3"
-                  value={to}
-                  on:input={handleInput}
-                  on:focus={handleFocus}
-                  on:change={handleCurrencyChangeTo}
-                  list="toCurrencyList"
-          />
-          <datalist id="toCurrencyList">
-            {#each currencies as option}
-              <option>{option}</option>
-            {/each}
-          </datalist>
-        </div>
+      </div>
+      <div class="calc__side">
+        <label class="calc__label" for="toCurrency">Convert To:</label>
+        <input
+                id="toCurrency"
+                class="calc__input"
+                name="toCurrency"
+                maxlength="3"
+                value={to}
+                on:input={handleInput}
+                on:focus={handleFocus}
+                on:change={handleCurrencyChangeTo}
+                list="toCurrencyList"
+        />
+        <datalist id="toCurrencyList">
+          {#each currencies as option}
+            <option>{option}</option>
+          {/each}
+        </datalist>
       </div>
     </div>
-    <footer>
-      <a href="https://www.exchangerate-api.com">Rates By Exchange Rate API</a>
-    </footer>
   </div>
+  <footer>
+    <a href="https://www.exchangerate-api.com">Rates By Exchange Rate API</a>
+  </footer>
 </main>
 
 <style>
+  .calc {
+    width: 100%;
+    padding: 1em;
+    box-sizing: border-box;
+    max-width: 768px;
+    min-width: 320px;
+    margin: 0 auto;
+    display: flex;
+    height: 40vh;
+    flex-direction: column;
+    text-align: center;
+    border: 1px solid transparent;
+    border-radius: 20px;
+    box-shadow: 0 5px 10px 0 rgb(0 0 0 / 70%);
+    background: #004225;
+    color: black;
+  }
+
+  .calc__wrapper_title {
+    box-sizing: border-box;
+    width: 100%;
+    margin: 1em auto;
+    padding-bottom: 3em;
+  }
+
+  .calc__wrapper_sides {
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    flex-grow: 1;
+  }
+
+  .calc__wrapper_side {
+    display: flex;
+    box-sizing: border-box;
+    flex-direction: column;
+    justify-content: left;
+    align-items: center;
+    height: 100%;
+  }
+
+  .calc__side {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    padding: 1em 0;
+  }
+
+  .calc__label {
+    position: absolute;
+    top: -10px;
+    left: 10px;
+    background: inherit;
+    color: #FFCF9D;
+    padding: 0 .5em;
+  }
+
+  .calc__input {
+    background: inherit;
+    color: #F5F5DC;
+    padding: 1em;
+    border-radius: 20px;
+    width: 200px;
+    border: 1px solid #FFB000;
+    outline: none;
+  }
+
+  .calc__input:focus {
+    outline: 1px solid #FFCF9D
+  }
+
 
 </style>
